56b98d6b31cdd4b3e4cbbe4c21d683d06e228dec
Puyan Wei <puyanwei@gmail.com>
2019-07-07 17:12:24 +0100

    Code for winning combination

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,14 +11,46 @@ const Board = () => {
 
     const handleClick = i => {
         const squaresValuesCopy = [...squaresValues];
+        if (calculateWinner(squaresValuesCopy) || squaresValuesCopy[i]) {
+            return;
+        }
         const symbol = xIsNext ? "X" : "O";
         squaresValuesCopy[i] = symbol;
         setSquaresValues(squaresValuesCopy);
         setxIsNext(!xIsNext);
     };
 
-    const symbol = xIsNext ? "X" : "O";
-    const status = `Next player: ${symbol}`;
+    const calculateWinner = squares => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (
+                squares[a] &&
+                squares[a] === squares[b] &&
+                squares[a] === squares[c]
+            ) {
+                return squares[a];
+            }
+        }
+        return null;
+    };
+
+    let status;
+
+    const winner = calculateWinner(squaresValues);
+
+    status = winner
+        ? `Winner: ${winner}`
+        : `Next player: ${xIsNext ? "X" : "O"}`;
 
     return (
         <div>
