951a277a5d975511537f070a99377914bbba1a72
Puyan Wei <puyanwei@gmail.com>
2019-07-07 16:29:16 +0100

    Adding state to board to keep track of square values

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+    const [squaresValues, setSquaresValues] = useState(Array(9).fill(null));
+
+    const renderSquare = i => <Square value={squaresValues[i]} />;
+
     const status = "Next player: X";
 
     return (
         <div>
             <div className="status">{status}</div>
             <div className="board-row">
-                <Square value={1} />
-                <Square value={2} />
-                <Square value={3} />
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div className="board-row">
-                <Square value={4} />
-                <Square value={5} />
-                <Square value={6} />
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
             <div className="board-row">
-                <Square value={7} />
-                <Square value={8} />
-                <Square value={9} />
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
         </div>
     );
