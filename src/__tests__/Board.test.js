37255508f0ede154452ba9311533e6dbf04a68a4
Puyan Wei <puyanwei@gmail.com>
2019-07-16 00:29:33 +0100

    Testing game ending, refactoring function on beforeEach

--- src/__tests__/Board.test.js
+++ src/__tests__/Board.test.js
@@ -6,7 +6,7 @@ import Board from "../components/Board";
 import Square from "../components/Square";
 
 /*
-What behaviors are there?
+What behaviors are there? Should I be including tests for the following?
 
 - Clicking a square will call handleClick
 - Clicking a square will call calculateWinner
@@ -18,11 +18,18 @@ What behaviors are there?
 - Test that once won, nothing is clickable
 - Test that the once won, the text displays 
 */
+let getElementByClassName;
+
+beforeEach(() => {
+    getElementByClassName = (name, arrayNumber) =>
+        document.getElementsByClassName(name)[arrayNumber];
+});
+
 describe("Board.js", () => {
     it("clicking the first square adds an 'X' to the button", () => {
         render(<Board />);
 
-        const squareOne = document.getElementsByClassName("square")[0];
+        const squareOne = getElementByClassName("square", 0);
 
         fireEvent.click(squareOne);
         expect(squareOne.innerHTML).toEqual("X");
@@ -30,8 +37,8 @@ describe("Board.js", () => {
     it("clicking the first and second squares adds an 'X' and 'O' to the first two buttons", () => {
         render(<Board />);
 
-        const squareOne = document.getElementsByClassName("square")[0];
-        const squareTwo = document.getElementsByClassName("square")[1];
+        const squareOne = getElementByClassName("square", 0);
+        const squareTwo = getElementByClassName("square", 1);
 
         fireEvent.click(squareOne);
         fireEvent.click(squareTwo);
@@ -41,14 +48,33 @@ describe("Board.js", () => {
     });
     it("clicking the first and second squares will toggle the text for what the next players symbol is", () => {
         render(<Board />);
-        const status = document.getElementsByClassName("status")[0];
-        const squareOne = document.getElementsByClassName("square")[0];
-        const squareTwo = document.getElementsByClassName("square")[1];
+        const status = getElementByClassName("status", 0);
+        const squareOne = getElementByClassName("square", 0);
+        const squareTwo = getElementByClassName("square", 1);
 
         expect(status.innerHTML).toEqual("Next player: X");
         fireEvent.click(squareOne);
         expect(status.innerHTML).toEqual("Next player: O");
         fireEvent.click(squareTwo);
         expect(status.innerHTML).toEqual("Next player: X");
+    });
+    it("Nothing is clickable once the game is won", () => {
+        render(<Board />);
+        const status = getElementByClassName("status", 0);
+        const squareOne = getElementByClassName("square", 0);
+        const squareTwo = getElementByClassName("square", 1);
+        const squareThree = getElementByClassName("square", 2);
+        const squareFour = getElementByClassName("square", 3);
+        const squareFive = getElementByClassName("square", 4);
+        const squareSix = getElementByClassName("square", 5);
+
+        fireEvent.click(squareOne);
+        fireEvent.click(squareFour);
+        fireEvent.click(squareTwo);
+        fireEvent.click(squareFive);
+        fireEvent.click(squareThree);
+        expect(status.innerHTML).toEqual("Winner: X");
+        fireEvent.click(squareSix);
+        expect(squareSix.innerHTML).toEqual("");
     });
 });
